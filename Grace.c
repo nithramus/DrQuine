dd78a2b65e3d31e51fd360baa33571984a991485
nithramir <batiste.andre53@hotmail.fr>
2018-05-14 21:05:34 +0200

    adding the 2 define

--- Grace.c
+++ Grace.c
@@ -4,5 +4,7 @@
 #include <fcntl.h>  
 #include <stdlib.h>
 #include <stdio.h>
-#define FT() int main(){FILE *file = fopen("Grace_kid.c", "w+");if (!file){exit(0);}char *first = "#include <stdio.h>%c#include <sys/types.h>%c#include <sys/stat.h>%c#include <fcntl.h>  %c#include <stdlib.h>%c#include <stdio.h>%c#define FT() int main(){FILE *file = fopen(%cGrace_kid.c%c, %cw+%c);if (!file){exit(0);}char *first = %c%s%c;fprintf(file, first,10,10,10,10,10,10,34,34,34,34,34,first,34,10);}%cFT();";fprintf(file, first,10,10,10,10,10,10,34,34,34,34,34,first,34,10);}
+#define USELESS 0
+#define SWAGMAN 1
+#define FT() int main(){FILE *file = fopen("Grace_kid.c", "w+");if (!file){exit(0);}char *first = "#include <stdio.h>%c#include <sys/types.h>%c#include <sys/stat.h>%c#include <fcntl.h>  %c#include <stdlib.h>%c#include <stdio.h>%c#define USELESS 0%c#define SWAGMAN 1%c#define FT() int main(){FILE *file = fopen(%cGrace_kid.c%c, %cw+%c);if (!file){exit(0);}char *first = %c%s%c;fprintf(file, first,10,10,10,10,10,10,10,10,34,34,34,34,34,first,34,10);}%cFT();";fprintf(file, first,10,10,10,10,10,10,10,10,34,34,34,34,34,first,34,10);}
 FT();
