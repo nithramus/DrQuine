f1c6aebea026561650a4a5180bcd52f5dbdb6670
nithramir <batiste.andre53@hotmail.fr>
2018-05-14 20:59:31 +0200

    first two program

---
+++ Grace.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>  
+#include <stdlib.h>
+#include <stdio.h>
+#define FT() int main(){FILE *file = fopen("Grace_kid.c", "w+");if (!file){exit(0);}char *first = "#include <stdio.h>%c#include <sys/types.h>%c#include <sys/stat.h>%c#include <fcntl.h>  %c#include <stdlib.h>%c#include <stdio.h>%c#define FT() int main(){FILE *file = fopen(%cGrace_kid.c%c, %cw+%c);if (!file){exit(0);}char *first = %c%s%c;fprintf(file, first,10,10,10,10,10,10,34,34,34,34,34,first,34,10);}%cFT();";fprintf(file, first,10,10,10,10,10,10,34,34,34,34,34,first,34,10);}
+FT();
